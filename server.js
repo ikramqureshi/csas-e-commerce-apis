13c780ca4e287f682bd53b641296fdd37fc4304e
Mohammed Soheb Shaik <Shaikmdshoaib737@gmail.com>
2025-04-28 15:15:44 -0500

    Shoaib Commented

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ app.get('/products',(req,res)=>{res.json({name:'tea_leaves', price:1000})})
 app.listen(3000,()=>{console.log(' Server Listening on Port no: 3000')})
 
 console.log("As Salamulalikum, This is Mohammed")
+console.log("Walaikum As Salam, I am Shoaib")
