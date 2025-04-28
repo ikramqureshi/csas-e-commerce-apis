87288ceffca4e3d558045279328866ffd4631e31
abdul-haq350 <abdul_haq350@yahoo.com>
2025-04-27 22:51:13 -0500

    Fifth commit by Developer: Abdul Haq

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require("express");
 const app = express()
+app.get('/products',(req,res)=>{res.json({name:'tea_leaves', price:1000})})
 
 app.listen(3000,()=>{console.log(' Server Listening on Port no: 3000')})
